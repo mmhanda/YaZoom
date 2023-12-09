bc4775a878d431118dc39a2cc1e2026f8b070e7e
mohammed <mohammedhanda109@gmail.com>
2023-12-09 17:39:37 +0100

    make new message container

--- frontend/src/RoomPage/ParticipantsSection/DirectChat/MessagesContainer.jsx
+++ frontend/src/RoomPage/ParticipantsSection/DirectChat/MessagesContainer.jsx
@@ -1,4 +1,4 @@
-import SingleMessage from "./SingleMessage";
+import { useEffect, useState } from "react";
 
 const SingleMessage = ({ messageContent, isAuthor }) => {
   const messageStyling = isAuthor ? 'author_direct_message' : 'receiver_direct_message';
@@ -16,7 +16,7 @@ const SingleMessage = ({ messageContent, isAuthor }) => {
 const MessagesContainer = ({ messages }) => {
   return (
     <div className="direct_messages_container">
-      {messages.map((message) => {
+      {messages?.map((message) => {
         <SingleMessage messageContent={message.messageContent}
           isAuthor={message.isAuthor}
           key={`${message.messageContent}-${message.identity}`} />
--- frontend/src/RoomPage/ParticipantsSection/DirectChat/NewMessage/NewMessage.jsx
+++ frontend/src/RoomPage/ParticipantsSection/DirectChat/NewMessage/NewMessage.jsx
@@ -1,7 +1,36 @@
-const NewMessage = ({ activeConversation, identity }) => {
-  return (
-    <div>
+import { useState } from "react";
+import SendMsgButton from "../../../../resources/images/sendMessageButton.svg";
 
+const NewMessage = ({ activeConversation, identity }) => {
+
+  const [message, setMessage] = useState('');
+  const handelTextChange = (event) => {
+    setMessage(event.target.value);
+  };
+
+  const sendMsg = () => {
+    if (message.length > 0 && message.replace(/\s/g, '').length) {
+      // WebRTCHandler.sendMessageUsingDataChannel(message);
+      setMessage('');
+    }
+  };
+
+  const handelKeyPressed = (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      sendMsg();
+    }
+  };
+  return (
+    <div className="new_message_container new_message_direct_border">
+      <input className="new_message_input"
+        value={message}
+        onChange={handelTextChange}
+        placeholder="Message"
+        onKeyDown={handelKeyPressed} />
+      <img className="new_message_button"
+        src={SendMsgButton}
+        onClick={sendMsg} alt="new_message" />
     </div>
   );
 };
